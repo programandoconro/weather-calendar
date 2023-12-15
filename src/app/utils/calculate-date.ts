36d63948b5b0cbd8bd1cb7cf9dcd08d5ddef08de
programandoconro <programandoconro@gmail.com>
2023-12-16 08:40:56 +0900

    try to fix server timezone

--- src/app/utils/calculate-date.ts
+++ src/app/utils/calculate-date.ts
@@ -1,10 +1,25 @@
 export default function calculateFromUTCDate(date: string) {
   const utcDate = new Date(date + "Z");
   const incomingDay: number = utcDate.getDate();
-  const currentDate = new Date().getUTCDate();
+  const utcNow = new Date();
+
+  // Create an Intl.DateTimeFormat object with Japanese locale and Asia/Tokyo time zone
+  const jstFormatter = new Intl.DateTimeFormat("ja-JP", {
+    timeZone: "Asia/Tokyo",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  });
+
+  // Format the UTC time in Japan Standard Time
+  const jstTimeString = jstFormatter.format(utcNow);
+  const currentDay = new Date(jstTimeString).getDate();
 
   return {
-    dayFromToday: incomingDay - currentDate,
+    dayFromToday: incomingDay - currentDay,
     localDate: utcDate,
   };
 }
