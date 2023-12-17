e0d4fb493ffa9db87e0e6790a1c4c839a9c46b97
programandoconro <programandoconro@gmail.com>
2023-12-17 10:16:57 +0900

    get initial data from server

---
+++ src/app/components/weather-calendar.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useEffect, useState } from "react";
+import { Weather } from "../model";
+import Day from "./day";
+import styles from "../page.module.css";
+
+export default function WeatherCalendar(props: { initialData: Weather }) {
+  const { initialData } = props;
+  const [weather, setWeather] = useState(initialData);
+
+  useEffect(() => {
+    async function fetchForecast() {
+      const response = await fetch("api/forecast");
+      const json = await response.json();
+
+      setWeather(json.data);
+    }
+    fetchForecast();
+    const fetchInterval = setInterval(() => fetchForecast(), 1000 * 60);
+
+    return () => clearInterval(fetchInterval);
+  }, []);
+
+  const days = [0, 1, 2, 3, 4].map((dayIndex) => {
+    return Day({ dayIndex, weather });
+  });
+
+  return (
+    <div>
+      <title>Weather Calendar</title>
+
+      <main className={styles.main}>
+        <div className={styles.calendar}>{days}</div>
+      </main>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
-import Day from "./components/day";
-import styles from "./page.module.css";
+import WeatherCalendar from "./components/weather-calendar";
 import { fetchWeather } from "./utils/fetch-weather";
 
 export default async function Home() {
   const weather = await fetchWeather();
 
-  const days = [0, 1, 2, 3, 4].map((dayIndex) => {
-    return Day({ dayIndex, weather });
-  });
-  return (
-    <div>
-      <title>Weather Calendar</title>
-
-      <main className={styles.main}>
-        <div className={styles.calendar}>{days}</div>
-      </main>
-    </div>
-  );
+  return <WeatherCalendar initialData={weather} />;
 }
