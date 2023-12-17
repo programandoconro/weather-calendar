1a633637e4c8860d0974e1534ce8dd6f98719373
programandoconro <programandoconro@gmail.com>
2023-12-17 19:35:16 +0900

    add modal on hover

--- src/app/components/forecast-card.tsx
+++ src/app/components/forecast-card.tsx
@@ -2,9 +2,21 @@ import { Forecast } from "../model";
 import Image from "next/image";
 import styles from "../page.module.css";
 import { weatherBackgroundColor } from "../utils/weather-background-color";
+import { useState } from "react";
 
 export default function ForecastBlock(props: { forecast: Forecast }) {
   const { forecast } = props;
+  const [isHover, setIsHover] = useState(false);
+  function onMouseEnter() {
+    setTimeout(() => {
+      setIsHover(true);
+    }, 300);
+  }
+  function onMouseLeave() {
+    setTimeout(() => {
+      setIsHover(false);
+    }, 300);
+  }
   return (
     <div
       className={styles.forecast}
@@ -20,11 +32,16 @@ export default function ForecastBlock(props: { forecast: Forecast }) {
         width={50}
         height={50}
         src={`http://openweathermap.org/img/w/${forecast.icon}.png`}
-        alt=""
+        alt="weather description image"
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
       />
 
       <h3>{forecast.temp} °C</h3>
       <h5>༄ {Math.floor(forecast.wind * 3.6)} Km/h</h5>
+      <dialog className={styles.dialog} open={isHover}>
+        {forecast.description[0].toUpperCase() + forecast.description.slice(1)}
+      </dialog>
     </div>
   );
 }
