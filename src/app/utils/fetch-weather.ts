662369b5f7aed4e6fa302d81831642bc8cfa8ad6
programandoconro <programandoconro@gmail.com>
2023-12-17 09:26:33 +0900

    refactor request

--- src/app/utils/fetch-weather.ts
+++ src/app/utils/fetch-weather.ts
@@ -1,15 +1,15 @@
 export async function fetchWeather() {
   const lat = process.env.LAT;
   const lon = process.env.LON;
-  const APPID = process.env.API_KEY;
+  const apiKey = process.env.API_KEY;
+
+  const query = `lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  const url = `http://api.openweathermap.org/data/2.5/forecast?${query}`;
 
   try {
-    const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APPID}&units=metric`,
-      {
-        cache: "no-store",
-      }
-    );
+    const response = await fetch(url, {
+      cache: "no-store",
+    });
     if (!response.ok) {
       throw new Error("failed to fetch");
     }
