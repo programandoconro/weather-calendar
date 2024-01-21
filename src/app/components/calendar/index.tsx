6ec95889e3a224fcc81d9b76982b4056533df3cd
programandoconro <programandoconro@gmail.com>
2024-01-21 19:50:46 +0900

    add title for weather sections

--- src/app/components/calendar/index.tsx
+++ src/app/components/calendar/index.tsx
@@ -61,7 +61,9 @@ export default function Calendar(props: {
   return (
     <div className={styles.calendar}>
       <div className={styles.card}>
+        <h2 className={styles.current}>今の天気：</h2>
         <CurrentWeatherCard currentWeather={currentWeather} />
+        <h1 className={styles.forecast}>天気予報</h1>
         {forecasts}
       </div>
     </div>
