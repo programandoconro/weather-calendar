fb7b5148b1fdad50e5e374a965945877fbd514c3
programandoconro <programandoconro@gmail.com>
2024-03-09 16:04:54 +0900

    more card colors depending on weather

--- src/app/utils/weather-background-color.ts
+++ src/app/utils/weather-background-color.ts
@@ -6,19 +6,34 @@ export function weatherBackgroundColor(
 ): CSSProperties["color"] {
   switch (weather) {
     case "Clear": {
-      return "#444499";
+      return "#345693"; // Dark Blue
     }
     case "Clouds": {
-      return "#444";
+      return "#444444"; // Dark Gray
     }
     case "Rain": {
-      return "#222";
+      return "#1F2F3F"; // Dark Slate Blue
     }
     case "Snow": {
-      return "gray";
+      return "#36454F"; // Darker Slate Gray
+    }
+    case "Thunderstorm": {
+      return "#1A2A3A"; // Very Dark Slate Blue
+    }
+    case "Mist": {
+      return "#2F2F2F"; // Darker Gray
+    }
+    case "Shower Rain": {
+      return "#293742"; // Very Dark Blue
+    }
+    case "Few Clouds": {
+      return "#3C3C3C"; // Slightly Darker Gray
+    }
+    case "Scattered Clouds": {
+      return "#2E2E2E"; // Darker Gray
     }
     default: {
-      return "darkred";
+      return "black"; // Default Color
     }
   }
 }
