d63101ab69ff30cb939a9e031bd3253a91900a79
programandoconro <programandoconro@gmail.com>
2024-08-16 12:37:31 +0900

    ask for enable location

--- src/app/ui/location-update-button/index.tsx
+++ src/app/ui/location-update-button/index.tsx
@@ -6,19 +6,32 @@ import Image from "next/image";
 export default function LocationUpdateButton() {
   const { setLocation } = useContext(LocationContext);
   const [isLoading, setIsLoading] = useState(false);
+
+  const successCallback = (loc: GeolocationPosition) => {
+    const { latitude, longitude } = loc.coords;
+    if (latitude && longitude) {
+      setLocation(String(latitude), String(longitude));
+      alert(
+        `位置情報は更新されました。緯度は：${latitude}です。軽度は：${longitude}です。`
+      );
+    } else {
+      alert("位置情報の取得は問題がありました。");
+    }
+    setIsLoading(false);
+  };
+
+  const errorCallback = (error: GeolocationPositionError) => {
+    if (error.PERMISSION_DENIED) {
+      alert(error.message + " 位置情報を有効にしてください");
+      setIsLoading(false);
+    }
+  };
+
   function onClick() {
     setIsLoading(true);
-    navigator.geolocation.getCurrentPosition((loc) => {
-      const { latitude, longitude } = loc.coords;
-      if (latitude && longitude) {
-        setLocation(String(latitude), String(longitude));
-        alert(
-          `位置情報は更新されました。緯度は：${latitude}です。軽度は：${longitude}です。`
-        );
-      } else {
-        alert("位置情報の取得は問題がありました。");
-      }
-      setIsLoading(false);
+
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {
+      maximumAge: 60000,
     });
   }
   return (
